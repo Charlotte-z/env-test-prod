32c50ee96e0322a57efc99a0b3843fbaf1dfd9e2
charlotte-z <2698028166@qq.com>
2023-02-05 22:44:25 +0800

    test

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,10 @@ import Sub from "./sub.jsx";
 
 const App = () => (
   <h1>
-    thisis{window.PRODUCTION}
+    <h2>env:</h2>
+    <p>process.env.PRODUCTION: {window.PRODUCTION}</p>
+    <p>process.env.VERSION: {window.VERSION}</p>
+    <p>process.env.PRODUCTS: {window.PRODUCTS}</p>
     <Sub></Sub>
   </h1>
 );
